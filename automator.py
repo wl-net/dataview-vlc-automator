12719d835810052fdcc7e52502c7ed4c7ed55fcf
Andrew Sorensen <andrew@andrewsorensen.net>
2016-04-10 21:42:25 -0700

    Style cleanup

--- automator.py
+++ automator.py
@@ -40,18 +40,18 @@ class MonitorThread(Thread):
 
 class DataviewVLCController(object):
     def __init__(self):
-      self.instance = vlc.Instance()
-      self.mp = self.instance.media_player_new()
-      self.ml = self.instance.media_list_new()
-      self.mlp = self.instance.media_list_player_new()
-      self.mlp.set_media_player(self.mp)
-      self.mlp.set_media_list(self.ml)
-      self.volume = 50
-      self.mp.audio_set_volume(self.volume)
-      self.monitor_stop_flag = Event()
-      self.monitor = MonitorThread(self.monitor_stop_flag, self)
-      self.url = None
-      self.previously_played = []
+        self.instance = vlc.Instance()
+        self.mp = self.instance.media_player_new()
+        self.ml = self.instance.media_list_new()
+        self.mlp = self.instance.media_list_player_new()
+        self.mlp.set_media_player(self.mp)
+        self.mlp.set_media_list(self.ml)
+        self.volume = 50
+        self.mp.audio_set_volume(self.volume)
+        self.monitor_stop_flag = Event()
+        self.monitor = MonitorThread(self.monitor_stop_flag, self)
+        self.url = None
+        self.previously_played = []
 
     def start_timers(self):
         if not self.monitor.is_alive() and not self.monitor_stop_flag.is_set():
@@ -82,48 +82,48 @@ class DataviewVLCController(object):
                 self.previously_played.append(current)
 
     def pause(self):
-      """
-      Pause the audio stream.
-      """
-      self.mp.set_pause(1)
-      return True
+        """
+        Pause the audio stream.
+        """
+        self.mp.set_pause(1)
+        return True
 
     def unpause(self):
-      """
-      Unpause the audio stream.
-      """
-      self.mp.set_pause(0)
-      return True
+        """
+        Unpause the audio stream.
+        """
+        self.mp.set_pause(0)
+        return True
 
     def set_volume(self, volume):
-      """
-      Sets the volume for the current player instance
-      """
-      self.volume = int(volume)
-      self.mp.audio_set_volume(self.volume)
+        """
+        Sets the volume for the current player instance
+        """
+        self.volume = int(volume)
+        self.mp.audio_set_volume(self.volume)
 
-      return True
+        return True
 
     def play(self, url):
-      self.url = url
-      m = vlc.Media(url)
+        self.url = url
+        m = vlc.Media(url)
 
-      if self.mp.get_media() is None or self.mp.get_media().get_mrl() != m.get_mrl():
-          self.ml.add_media(m)
-          self.mlp.play_item(m)
-          self.mp.audio_set_volume(self.volume)
+        if self.mp.get_media() is None or self.mp.get_media().get_mrl() != m.get_mrl():
+            self.ml.add_media(m)
+            self.mlp.play_item(m)
+            self.mp.audio_set_volume(self.volume)
 
-      self.start_timers()
+        self.start_timers()
 
-      # hack to set the default volume
-      while True:
-          if self.mp.audio_get_volume() is not -1:
-              self.mp.audio_set_volume(self.volume)
-              return True
+        # hack to set the default volume
+        while True:
+            if self.mp.audio_get_volume() is not -1:
+                self.mp.audio_set_volume(self.volume)
+                return True
 
     def mute(self):
-      self.mp.audio_set_mute(True)
-      return True
+        self.mp.audio_set_mute(True)
+        return True
 
     def unmute(self):
       self.mp.audio_set_mute(False)
@@ -153,7 +153,7 @@ class DataviewRPCServer(aiohttp.server.ServerHttpProtocol):
         self.dispatch_functions = dispatch_functions
         self.auth_token = auth_token
         if len(auth_token) < 32:
-            raise Exception("auth_token is insufficently long")
+            raise Exception('auth_token is insufficently long')
         super().__init__()
 
     @asyncio.coroutine
